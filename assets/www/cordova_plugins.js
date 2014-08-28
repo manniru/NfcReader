6886ab816cc5cbeffd758ce7f46a2dc1b7aeed5c
MUHAMMAD MANNIR AHMAD <manniru@gmail.com>
2014-08-28 16:08:58 +0800

    Initialize Source Files

---
+++ assets/www/cordova_plugins.js
@@ -0,0 +1,39 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/com.chariotsolutions.nfc.plugin/www/phonegap-nfc.js",
+        "id": "com.chariotsolutions.nfc.plugin.NFC",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.blackberry.invoke/www/client.js",
+        "id": "com.blackberry.invoke.client",
+        "clobbers": [
+            "blackberry.invoke"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.device": "0.2.4",
+    "org.apache.cordova.vibration": "0.3.4",
+    "com.chariotsolutions.nfc.plugin": "0.4.6",
+    "com.blackberry.invoke": "1.0.0"
+}
+// BOTTOM OF METADATA
+});
